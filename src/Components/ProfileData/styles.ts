6057bc2b837767fe21db613c3ef717eb0a576a75
Marcos Botene <marcosjbotene@gmail.com>
2021-01-18 11:43:43 -0300

    Styles for Profile component added.

---
+++ src/Components/ProfileData/styles.ts
@@ -0,0 +1,120 @@
+import styled, { css } from "styled-components";
+
+import {
+  GoOrganization,
+  GoLocation,
+  GoMail,
+  GoLink,
+  GoPerson,
+} from "react-icons/go";
+
+export const Container = styled.div``;
+
+export const Flex = styled.div`
+  display: flex;
+  align-items: center;
+
+  > div {
+    margin-left: 24px;
+
+    > h1 {
+      font-size: 26px;
+      line-height: 1.25;
+      color: #24292e;
+      font-weight: 600;
+    }
+
+    > h2 {
+      font-size: 20px;
+      color: #666;
+      font-weight: 300;
+    }
+  }
+
+  @media (min-width: 768px) {
+    flex-direction: column;
+    align-items: flex-start;
+
+    > div {
+      margin-left: 0;
+      margin-top: 16px;
+    }
+  }
+`;
+
+export const Avatar = styled.img`
+  width: 16%;
+  border-radius: 50%;
+
+  @media (min-width: 768px) {
+    width: 100%;
+    margin-top: 38px;
+  }
+`;
+
+export const Row = styled.ul`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  margin: 20px 0;
+
+  > li {
+    display: flex;
+    align-items: center;
+
+    > span {
+      font-size: 14px;
+      color: #586069;
+    }
+
+    > * {
+      margin-right: 5px;
+    }
+  }
+`;
+
+const iconCSS = css`
+  width: 16px;
+  height: 16px;
+  fill: #6a737d;
+  flex-shrink: 0;
+`;
+
+export const PeopleIcon = styled(GoPerson)`
+  ${iconCSS}
+`;
+
+export const Column = styled.ul`
+  li {
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+  }
+
+  li + li {
+    margin-top: 10px;
+  }
+
+  span {
+    margin-left: 5px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+`;
+
+export const CompanyIcon = styled(GoOrganization)`
+  ${iconCSS}
+`;
+
+export const LocationIcon = styled(GoLocation)`
+  ${iconCSS}
+`;
+
+export const EmailIcon = styled(GoMail)`
+  ${iconCSS}
+`;
+
+export const BlogIcon = styled(GoLink)`
+  ${iconCSS}
+`;
